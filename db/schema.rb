42b51aceeef2b5ca2cd65e43860eddbb401ed7af
Janaka Vithanage <jkvithana@gmail.com>
2023-01-26 23:51:51 +1100

    Feedback model created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_060813) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_123638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_060813) do
     t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "uniqid"
+    t.integer "score"
+    t.text "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.json "invoice"
+    t.json "product"
   end
 
   create_table "users", force: :cascade do |t|
